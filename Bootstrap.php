3c0e039bf5dc37a8774f94ca4a1a354dbc7fc9ef
Veselov on MacBookPro <pvsaintpe@mail.ru>
2018-04-10 23:03:01 +0000

    add Bootstrap

---
+++ Bootstrap.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace pvsaintpe\log;
+
+use yii\base\BootstrapInterface;
+use Yii;
+
+/**
+ * Class Bootstrap
+ * @package pvsaintpe\log
+ */
+class Bootstrap implements BootstrapInterface
+{
+    /**
+     * @param \yii\base\Application $app
+     */
+    public function bootstrap($app)
+    {
+    }
+}
