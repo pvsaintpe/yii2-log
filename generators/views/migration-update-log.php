446b1571ab941c7f7aa9b37f22a6b2304a33a4b8
Veselov Pavel <pvsaintpe@icloud.com>
2018-11-09 17:09:58 +0300

    add fk: cascade delete

--- generators/views/migration-update-log.php
+++ generators/views/migration-update-log.php
@@ -100,7 +100,8 @@ class <?= $className ?> extends Migration
             echo "\n            '{$logTableName}',";
             echo "\n            ['{$column}'],";
             echo "\n            " . '$this->getStorageDb()->getName()' . " . '.{$key['relation_table']}',";
-            echo "\n            '{$key['relation_column']}'";
+            echo "\n            '{$key['relation_column']}',";
+            echo "\n            static::CASCADE";
             echo "\n        );\n";
         }
     }
